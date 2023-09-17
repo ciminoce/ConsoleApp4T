0144f4b88565801428839904cc6192a7f89b88a5
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2023-09-17 17:09:57 -0300

    Agreagado de Fa a Ce

--- ConsoleApp4t/Program.cs
+++ ConsoleApp4t/Program.cs
@@ -6,8 +6,13 @@
         {
             Console.WriteLine("Hello, World!");
             var celsius = 20;
+            var fah = 69;
             Console.WriteLine(ConvertToFah(celsius));
+            Console.WriteLine(ConvertToCelsius(fah));
         }
+
+        private static double ConvertToCelsius(int fah) => (fah - 32) / 1.8f;
+        
 
         private static double ConvertToFah(double celsius) => 1.8 * celsius + 32;
         
