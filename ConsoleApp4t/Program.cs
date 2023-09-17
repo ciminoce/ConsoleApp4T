f7fd7c73f0282d6398d08769964c3e75fc43b6aa
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2023-09-17 17:05:59 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp4t/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp4t
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var celsius = 20;
+            Console.WriteLine(ConvertToFah(celsius));
+        }
+
+        private static double ConvertToFah(double celsius) => 1.8 * celsius + 32;
+        
+    }
+}
